94d02f378a7c8541daad2be9222cc9bdad9a871d
AMP2024 <mail30@laposte.net>
2024-07-17 03:46:45 +0200

    Add CRUD operations for Person entity

--- src/main/java/com/amp/safetynetalerts/controller/SafetynetalertsController.java
+++ src/main/java/com/amp/safetynetalerts/controller/SafetynetalertsController.java
@@ -1,8 +1,84 @@
 package com.amp.safetynetalerts.controller;
 
+import com.amp.safetynetalerts.dto.*;
+import com.amp.safetynetalerts.model.*;
+import com.amp.safetynetalerts.service.PersonService;
+import org.springframework.web.bind.annotation.RestController;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
 @RestController
 public class SafetynetalertsController {
 
+    private static final Logger logger = LogManager.getLogger(SafetynetalertsController.class);
+    private final FirestationService firestationService;
+    private final PersonService personService;
+    private final MedicalRecordService medicalRecordService;
 
+    public SafetynetalertsController(FirestationService firestationService,
+                                     PersonService personService,
+                                     MedicalRecordService medicalRecordService) {
+        this.firestationService = firestationService;
+        this.personService = personService;
+        this.medicalRecordService = medicalRecordService;
+    }
+
+    @GetMapping("/person")
+    public ResponseEntity<?> getPerson(@RequestParam String firstName, @RequestParam String lastName) throws Exception {
+
+        logger.info("Retrieving person with firstname: {}, lastname: {}", firstName, lastName);
+
+        PersonDTO person = personService.fetchPerson(firstName, lastName);
+
+        logger.debug("Retrieved person data: {}", person);
+
+        return new ResponseEntity<>(person, HttpStatus.OK);
+    }
+
+    @PostMapping(path = "/person", consumes = "application/json")
+    public ResponseEntity<?> addPerson(@RequestBody Person person) throws Exception {
+
+        PersonDTO savedPerson = null;
+
+        logger.info("Adding person: {}", person);
+
+        savedPerson = personService.addAndPersistPerson(person);
+
+        logger.info("Person successfully added: {}", savedPerson);
+
+        return new ResponseEntity<>(savedPerson, HttpStatus.CREATED);
 
     }
+
+    @PutMapping(path = "/person", consumes = "application/json")
+    public ResponseEntity<?> updatePerson(@RequestBody Person person) throws Exception {
+
+        logger.info("Updating the person : {}", person);
+
+        personService.updatePersonDataWrapper(person);
+
+        logger.debug("Person successfully updated : {}", person);
+
+        return new ResponseEntity<>(HttpStatus.OK);
+
+    }
+
+    @DeleteMapping("/person")
+    public ResponseEntity<String> deletePerson(@RequestParam String firstName, @RequestParam String lastName) throws Exception {
+
+        logger.info("Deleting person with first name: {}, last name: {}", firstName, lastName);
+
+        personService.deletePersonDataWrapper(firstName, lastName);
+
+        logger.debug("Person successfully deleted.");
+
+        return new ResponseEntity<>("Person successfully deleted.", HttpStatus.NO_CONTENT);
+
+    }
+
+}
--- src/main/java/com/amp/safetynetalerts/service/PersonService.java
+++ src/main/java/com/amp/safetynetalerts/service/PersonService.java
@@ -1,6 +1,145 @@
 package com.amp.safetynetalerts.service;
 
+import com.amp.safetynetalerts.dto.*;
+import com.amp.safetynetalerts.exception.PersonDeleteException;
+import com.amp.safetynetalerts.exception.PersonUpdateException;
+import com.amp.safetynetalerts.model.DataWrapper;
+import com.amp.safetynetalerts.model.Person;
+
+
+import java.io.IOException;
+import java.util.*;
+
+import com.amp.safetynetalerts.repository.DataWrapperRepository;
+import lombok.Data;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+
+@Data
 @Service
 public class PersonService {
 
+    public void addPerson(List<Person> persons, String firstName, String lastName, String address, String city, String zip, String phone, String email) {
+
+        Person person = new Person();
+        if (firstName != null) {
+            person.setFirstName(firstName);
+        }
+        if (lastName != null) {
+            person.setLastName(lastName);
+        }
+        if (address != null) {
+            person.setAddress(address);
+        }
+        if (city != null) {
+            person.setCity(city);
+        }
+        if (zip != null) {
+            person.setZip(zip);
+        }
+        if (phone != null) {
+            person.setPhone(phone);
+        }
+        if (email != null) {
+            person.setEmail(email);
+        }
+        persons.add(person);
+    }
+
+    public void updatePerson(List<Person> persons, String firstName, String lastName, String address, String city, String zip, String phone, String email) {
+
+        boolean personFound = false;
+
+        for (Person person : persons) {
+            if (person.getFirstName().equals(firstName) && person.getLastName().equals(lastName)) {
+                personFound = true;
+                if (address != null) {
+                    person.setAddress(address);
+                }
+                if (city != null) {
+                    person.setCity(city);
+                }
+                if (zip != null) {
+                    person.setZip(zip);
+                }
+                if (phone != null) {
+                    person.setPhone(phone);
+                }
+                if (email != null) {
+                    person.setEmail(email);
+                }
+                break;
+            }
+        }
+
+        if (!personFound) {
+            throw new PersonUpdateException("Person not found");
+        }
+    }
+
+    public void deletePerson(List<Person> persons, String firstName, String lastName) {
+
+        boolean isDeleted = persons.removeIf(p -> p.getFirstName().equals(firstName) && p.getLastName().equals(lastName));
+
+        if (!isDeleted) {
+            throw new PersonDeleteException("No person was deleted with the provided user details!");
+        }
+    }
+
+    public static Person getPerson(List<Person> persons, String firstName, String lastName) throws NoHandlerFoundException {
+
+        for (Person person : persons) {
+            if (person.getFirstName().equals(firstName) && person.getLastName().equals(lastName)) {
+                return person;
+            }
+        }
+        throw new NoHandlerFoundException("GET", "/" + firstName + "/" + lastName, null);
+    }
+
+    public static PersonDTO fetchPerson(String firstName, String lastName) throws NoHandlerFoundException {
+
+        DataWrapper dataWrapper = DataWrapperRepository.getDataWrapper();
+        return PersonMapper.toPersonDTO(getPerson(dataWrapper.getPersons(), firstName, lastName));
+    }
+
+    public PersonDTO addAndPersistPerson(Person person) throws IOException {
+
+        DataWrapper dataWrapper = DataWrapperRepository.getDataWrapper();
+        addPerson(
+                dataWrapper.getPersons(),
+                person.getFirstName(),
+                person.getLastName(),
+                person.getAddress(),
+                person.getCity(),
+                person.getZip(),
+                person.getPhone(),
+                person.getEmail()
+        );
+        DataWrapperRepository.updateFileWithDataWrapper(dataWrapper);
+        return PersonMapper.toPersonDTO(person);
+    }
+
+    public void updatePersonDataWrapper(Person person) throws IOException {
+
+        DataWrapper dataWrapper = DataWrapperRepository.getDataWrapper();
+        updatePerson(dataWrapper.getPersons(),
+                person.getFirstName(),
+                person.getLastName(),
+                person.getAddress(),
+                person.getCity(),
+                person.getZip(),
+                person.getPhone(),
+                person.getEmail());
+        DataWrapperRepository.updateFileWithDataWrapper(dataWrapper);
+    }
+
+    public void deletePersonDataWrapper(String firstName, String lastName) throws IOException {
+
+        DataWrapper dataWrapper = DataWrapperRepository.getDataWrapper();
+        deletePerson(dataWrapper.getPersons(), firstName, lastName);
+        DataWrapperRepository.updateFileWithDataWrapper(dataWrapper);
+
+    }
 }
